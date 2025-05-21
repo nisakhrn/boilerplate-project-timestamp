ff3b4b679b1ca2c3db4b6b807bfcb549f26e3b0e
khairun nisa <khairunnn@msh.usk.ac.id>
2025-05-21 21:16:18 +0700

    up

--- index.js
+++ index.js
@@ -1,32 +1,51 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Timestamp Microservice</title>
+    </head>
+    <body style="font-family: sans-serif; text-align: center; padding: 30px;">
+      <h1>Timestamp Microservice</h1>
+      <hr>
+      <h3>Example Usage:</h3>
+      <p><a href="/api/2015-12-25">[project url]/api/2015-12-25</a></p>
+      <p><a href="/api/1451001600000">[project url]/api/1451001600000</a></p>
+      <h3>Example Output:</h3>
+      <code>{"unix": 1451001600000, "utc": "Fri, 25 Dec 2015 00:00:00 GMT"}</code>
+      <br><br>
+      <p>By <a href="https://www.freecodecamp.org" target="_blank">freeCodeCamp.org</a></p>
+    </body>
+    </html>
+  `);
 });
 
+app.get('/api/:date?', (req, res) => {
+  const dateParam = req.params.date;
+  let date;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  if (!dateParam) {
+    date = new Date();
+  } else if (!isNaN(dateParam)) {
+    date = new Date(parseInt(dateParam));
+  } else {
+    date = new Date(dateParam);
+  }
+
+  if (date.toString() === "Invalid Date") {
+    res.json({ error: "Invalid Date" });
+  } else {
+    res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
 });
 
-
-
-// Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
